016c74500167e4ea1715c75cbc6d0932d4a37c2f
Nancy Yao <Nancy@Nancys-MacBook-Air.local>
2016-10-08 10:35:51 -0400

    Changes to map vc

--- SafeTravel/MapViewController.swift
+++ SafeTravel/MapViewController.swift
@@ -8,22 +8,18 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 import CoreLocation
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
-    override func loadView() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         
         let camera = GMSCameraPosition.camera(withLatitude: 1.285, longitude: 103.848, zoom: 12)
         let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
         self.view = mapView
-
-    }
-    var locationManager = CLLocationManager()
-    var vwGMap = GMSMapView()
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
+        
+        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
         
@@ -42,31 +38,58 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         // Add GMSMapView to current view
         self.view = vwGMap
     }
+    var locationManager = CLLocationManager()
+    var vwGMap = GMSMapView()
+    
+    
+    // Present the Autocomplete view controller when the button is pressed.
+    @IBAction func originClicked(sender: AnyObject) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        self.present(autocompleteController, animated: true, completion: nil)
+    }
+    @IBAction func destinationClicked(sender: AnyObject) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        self.present(autocompleteController, animated: true, completion: nil)
+    }
+}
 
-    
-    
-    
-    
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension MapViewController: GMSAutocompleteViewControllerDelegate {
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: ", place.name)
+        print("Place address: ", place.formattedAddress)
+        print("Place attributions: ", place.attributions)
+        print("Place coordinates (CLLocationCoordinate2D)", place.coordinate)
+        //save in Route object as origin or destination!
+        
+        //mark point in Map
+        let position = place.coordinate
+        let marker = GMSMarker(position: position)
+        marker.title = "Hello World"
+        marker.icon = GMSMarker.markerImage(with: UIColor.blue)
+        marker.map = vwGMap //add to existing GMSMapView object
+        
+        self.dismiss(animated: true, completion: nil)
     }
     
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error)
+    }
     
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    // MARK: - CLLocationManagerDelegate
-    //1
-    
+    func didUpdateAutocompletePredictions(viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
 }
