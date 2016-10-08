5dc137a7b637c9c744663bf2278b0e5bf2ef3a1e
Nancy Yao <Nancy@nancys-macbook-air.local>
2016-10-08 06:46:24 -0400

    Initial Commit

---
+++ SafeTravel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafeTravel
+//
+//  Created by Nancy Yao on 10/8/16.
+//  Copyright © 2016 MHacks8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
